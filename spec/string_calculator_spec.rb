c57515cd91bf1111af87d12b9b5ade0d33276b83
Lucky Bhagat <lucky.bhagat00@gmail.com>
2025-07-16 12:01:48 +0530

    Add: raise error with message for multiple negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -41,4 +41,9 @@ RSpec.describe "StringCalculator" do
     expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
   end
 
+  it "raises an error listing all negative numbers" do
+    calculator = StringCalculator.new
+    expect { calculator.add("-1,-2,3") }.to raise_error("negative numbers not allowed: -1, -2")
+  end
+
 end
